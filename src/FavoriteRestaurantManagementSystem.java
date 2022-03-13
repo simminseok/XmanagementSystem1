c6570cc0296172a1e3cecac15b06a12658100be3
simminseok <tlaalstjr555@naver.com>
2022-03-13 20:02:37 +0900

    MenuManager.java -> FavoriteRestaurantManagementSystem.java

---
+++ src/FavoriteRestaurantManagementSystem.java
@@ -0,0 +1,13 @@
+
+public class FavoriteRestaurantManagementSystem {
+
+	public static void main(String[] args) {
+        System.out.println("1. Add Restaurant");
+        System.out.println("2. Delete Restaurant");
+        System.out.println("3. Edit Restaurant");
+        System.out.println("4. View Restaurant");
+        System.out.println("5. Exit");
+
+	}
+
+}
